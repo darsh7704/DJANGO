30b7bb646efd79a785236507bf948914ec126991
darsh7704 <darshanm772004@gmail.com>
2024-10-22 10:07:35 +0530

    Add files via upload
    A django project to generate passwords

---
+++ password_generater/generator/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import random
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+
+    length = int(request.GET.get('length', 12))
+    generated_password = ''.join(random.choice(characters) for _ in range(length))
+
+    return render(request, 'generator/password.html', {'password': generated_password})
